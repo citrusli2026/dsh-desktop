22150b74c7d66b9a0f8d05066556f3c1f7d03459
citrusli <citrusli@126.com>
2026-08-30 18:05:37 +0800

    feat(site): clarify shell positioning and support paths

--- site/assets/app.js
+++ site/assets/app.js
@@ -4,7 +4,7 @@
    - 平台识别 CTA / 复制 / 滚动 reveal
    数据与文案字典见 ./data-model.js (纯数据层,亦被 check-site 直接导入)。
    零依赖,渐进增强。 */
-import { I18N, platformOf, publicKind, splitCompositeTag, fmtSize, fmtNum, fmtDate, normalizeReleasesPayload } from './data-model.js?v=34'
+import { I18N, platformOf, publicKind, splitCompositeTag, fmtSize, fmtNum, fmtDate, normalizeReleasesPayload } from './data-model.js?v=36'
 
 var REPO = 'citrusli2026/dsh-desktop'
 var RELEASES_URL = 'https://github.com/' + REPO + '/releases'
@@ -472,7 +472,7 @@ function bindSectionSpy() {
       }
     })
   }, { rootMargin: '-72px 0px -72% 0px', threshold: 0 })
-  ;['workflow', 'download', 'features', 'faq'].forEach(function (id) {
+  ;['workflow', 'download', 'features', 'market', 'faq'].forEach(function (id) {
     var el = document.getElementById(id)
     if (el) spy.observe(el)
   })
